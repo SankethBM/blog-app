2d026207afbb2d5b02a5899b3f1fdcb54326737a
Sanketh B M <117658299+SankethBM@users.noreply.github.com>
2023-06-11 19:13:26 +0530

    client
    client

---
+++ components/topbar/Topbar.jsx
@@ -0,0 +1,66 @@
+// import { Link } from "react-router-dom";
+import React from "react";
+import "./topbar.css";
+import { Link } from "react-router-dom";
+import { Context } from "../../context/Context";
+import { useContext } from "react";
+
+export default function Topbar() {
+    const { user, dispatch } = useContext(Context);
+    const PF = "http://localhost:5000/images/"
+
+    const handleLogout = () => {
+        dispatch({ type: "LOGOUT" });
+    };
+
+    return (
+        <div className="top">
+            <div className="topLeft">
+                <i className="topIcon fab fa-facebook-square"></i>
+                <i className="topIcon fab fa-instagram-square"></i>
+                <i className="topIcon fab fa-whatsapp-square"></i>
+                <i className="topIcon fab fa-twitter-square"></i>
+            </div>
+
+            <div className="topCenter">
+                <ul className="topList">
+                    <li className="topListItem">
+                        <Link className="link" to='/'>HOME</Link>
+                    </li>
+                    <li className="topListItem"><Link className="link" to='/about'>ABOUT</Link></li>
+                    <li className="topListItem"><Link className="link" to='/contact'>CONTACT</Link></li>
+                    <li className="topListItem">
+                        <Link className="link" to='/write'>WRITE</Link>
+                    </li>
+                    <li className="topListItem" onClick={handleLogout}>
+                        {user && 'LOGOUT'}
+                    </li>
+                </ul>
+            </div>
+
+            <div className="topRight">
+                {user ? (
+                    <Link to='/settings' className="link">
+                        <img
+                            className="topImg"
+                            src={PF + user.profilePic}
+                            alt=""
+                        />
+                    </Link>
+                ) : (
+                    <ul className="topList">
+                        <li className="topListItem">
+                            <Link className="link" to='/login'>LOGIN</Link>
+                        </li>
+                        <li className="topListItem">
+                            <Link className="link" to='/register'>REGISTER</Link>
+                        </li>
+                    </ul>
+                )
+                }
+                <i className="topSearchIcon fas fa-search"></i>
+            </div>
+        </div>
+    )
+}
+
